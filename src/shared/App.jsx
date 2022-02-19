046673ea35076b38943c94ff35987de1c7fa3156
hyemin <hmwoo94@gmail.com>
2022-02-19 14:15:37 +0900

    변경사항 : pull conflict 수정

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -4,8 +4,8 @@ import { Route } from "react-router-dom";
 import { history } from "../redux/configureStore";
 import { ConnectedRouter } from "connected-react-router";
 import { useDispatch } from "react-redux";
-import { getCookie } from "./cookie"
-import { actionCreator as userActions } from "../redux/modules/user"
+import { getCookie } from "./cookie";
+import { actionCreator as userActions } from "../redux/modules/user";
 
 /* PAGE */
 import Landing from "../pages/Landing";
@@ -36,21 +36,21 @@ function App() {
   return (
     <div className="App">
       <Header />
-        <ConnectedRouter history={history}>
-          <div className="container">
-            <Sidebar />
-            <Route path="/" exact component={Landing} />
-            <Route path="/signup" exact component={Signup} />
-            <Route path="/signin" exact component={Signin} />
-            <Route path="/manage_profiles" exact component={ManageProfile} />
-            <Route path="/browse/video" exact component={Main} />
-            <Route path="/wishes" exact component={Wishes} />
-            <Route path="/watchings" exact component={Watchings} />
-            <Route path="/watched" exact component={Watched} />
-            <Route path="/ratings" exact component={Ratings} />
-            <Footer />
-          </div>
-        </ConnectedRouter>
+      <ConnectedRouter history={history}>
+        <div className="container">
+          <Sidebar />
+          <Route path="/" exact component={Landing} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/signin" exact component={Signin} />
+          <Route path="/manage_profiles" exact component={ManageProfile} />
+          <Route path="/browse/video" exact component={Main} />
+          <Route path="/wishes" exact component={Wishes} />
+          <Route path="/watchings" exact component={Watchings} />
+          <Route path="/watched" exact component={Watched} />
+          <Route path="/ratings" exact component={Ratings} />
+          <Footer />
+        </div>
+      </ConnectedRouter>
     </div>
   );
 }
